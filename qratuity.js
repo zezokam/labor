0edb8bafa2087d4ce0c0b5ae2770fcfc10f1acf6
zezokam <125836314+zezokam@users.noreply.github.com>
2023-03-10 00:41:05 +0400

    Create qratuity.js

---
+++ qratuity.js
@@ -0,0 +1,26 @@
+function calculateGratuity() {
+	// Get input values
+	var startDate = new Date(document.getElementById("startDate").value);
+	var endDate = new Date(document.getElementById("endDate").value);
+	var lastBasicWage = parseInt(document.getElementById("lastBasicWage").value);
+
+	// Calculate years of service
+	var yearsOfService = (endDate - startDate) / (1000 * 60 * 60 * 24 * 365);
+
+	// Calculate gratuity rate
+	var gratuityRate;
+	if (yearsOfService <= 3) {
+		gratuityRate = 15;
+	} else {
+		gratuityRate = 30;
+	}
+
+	// Calculate fraction of year of service
+	var fractionOfYear = (endDate - startDate) % (1000 * 60 * 60 * 24 * 365) / (1000 * 60 * 60 * 24 * 365);
+
+	// Calculate gratuity amount
+	var gratuityAmount = (gratuityRate / 30) * lastBasicWage * (yearsOfService + fractionOfYear);
+
+	// Display result
+	document.getElementById("result").innerHTML = "The post service gratuity is: " + gratuityAmount.toFixed(2) + " OMR";
+}
