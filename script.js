83ce8f9d7fc3e2eb29c83e4f2bb9d86693e33786
zezokam <125836314+zezokam@users.noreply.github.com>
2023-03-09 14:24:55 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 function calculateGratuity() {
-	const start_date = new Date(document.getElementById("start_date").value);
-	const end_date = new Date(document.getElementById("end_date").value);
-	const monthly_salary = parseInt(document.getElementById("monthly_salary").value);
+    const start_date = new Date(document.getElementById("start_date").value);
+    const end_date = new Date(document.getElementById("end_date").value);
+    const monthly_salary = parseFloat(document.getElementById("monthly_salary").value).toFixed(3);
 
-	const daily_salary = monthly_salary / 30;
-	const years_of_service = Math.floor((end_date - start_date) / (1000 * 60 * 60 * 24 * 365));
+    const daily_salary = monthly_salary / 30;
+    const years_of_service = Math.floor((end_date - start_date) / (1000 * 60 * 60 * 24 * 365));
 
-	let post_service_gratuity = 0;
+    let post_service_gratuity = 0;
 
-	if (years_of_service >= 1) {
-		if (years_of_service <= 3) {
-			post_service_gratuity = years_of_service * 15 * daily_salary;
-		} else {
-			post_service_gratuity = 3 * 15 * daily_salary;
-			post_service_gratuity += (years_of_service - 3) * 30 * daily_salary;
-		}
-	}
+    if (years_of_service >= 1) {
+        if (years_of_service <= 3) {
+            post_service_gratuity = years_of_service * 15 * daily_salary;
+        } else {
+            post_service_gratuity = 3 * 15 * daily_salary;
+            post_service_gratuity += (years_of_service - 3) * 30 * daily_salary;
+        }
+    }
 
-	document.getElementById("result").innerHTML = "Post-Service Gratuity: " + post_service_gratuity.toFixed(2) + " O.R";
+    document.getElementById("result").innerHTML = "Post-Service Gratuity: " + post_service_gratuity.toFixed(3) + " O.R";
 }
