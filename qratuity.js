7574085549ce171473cd083771ee2d43339a849b
zezokam <125836314+zezokam@users.noreply.github.com>
2023-03-10 01:46:02 +0400

    Update qratuity.js

--- qratuity.js
+++ qratuity.js
@@ -20,7 +20,7 @@ function calculateGratuity() {
     if (yearsOfService < 3) {
         gratuityAmount = (dailyWage * 15 * yearsOfService);
     } else {
-        gratuityAmount = (dailyWage * 30 * yearsOfService) + (dailyWage * 30 / 12) * monthsOfService);
+        gratuityAmount = (dailyWage * 30 * yearsOfService) + (dailyWage * 30 * monthsOfService / 12);
     }
 
     // Display result
