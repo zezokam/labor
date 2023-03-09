d219da661efc63fe566b0a645c3ea40c5b6588d3
zezokam <125836314+zezokam@users.noreply.github.com>
2023-03-09 05:31:41 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
-function calculate() {
-  const startDate = new Date(document.getElementById("startDate").value);
-  const endDate = new Date(document.getElementById("endDate").value);
-  const salary = parseFloat(document.getElementById("salary").value);
+function calculateGratuity() {
+	const start_date = new Date(document.getElementById("start_date").value);
+	const end_date = new Date(document.getElementById("end_date").value);
+	const monthly_salary = parseInt(document.getElementById("monthly_salary").value);
 
-  const daysWorked = (endDate - startDate) / (1000 * 60 * 60 * 24);
-  const yearsWorked = daysWorked / 365;
-  const dailySalary = salary / 30;
+	const daily_salary = monthly_salary / 30;
+	const years_of_service = Math.floor((end_date - start_date) / (1000 * 60 * 60 * 24 * 365));
 
-  let gratuity = 0;
-  let steps = "";
+	let post_service_gratuity = 0;
 
-  if (yearsWorked < 1) {
-    steps = "البدل التقاعدي غير مطبق على مدة
+	if (years_of_service >= 1) {
+		if (years_of_service <= 3) {
+			post_service_gratuity = years_of_service * 15 * daily_salary;
+		} else {
+			post_service_gratuity = 3 * 15 * daily_salary;
+			post_service_gratuity += (years_of_service - 3) * 30 * daily_salary;
+		}
+	}
+
+	document.getElementById("result").innerHTML = "Post-Service Gratuity: " + post_service_gratuity.toFixed(2) + " O.R";
+}
