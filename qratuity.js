61c91bc7005dd06993af59bf82699156b3359c70
zezokam <125836314+zezokam@users.noreply.github.com>
2023-03-10 00:58:24 +0400

    Update qratuity.js

--- qratuity.js
+++ qratuity.js
@@ -1,29 +1,24 @@
 function calculateGratuity() {
-	// Get input values
-	var startDate = new Date(document.getElementById("startDate").value);
-	var endDate = new Date(document.getElementById("endDate").value);
-	var monthlySalary = parseInt(document.getElementById("monthlySalary").value);
+  // Get input values
+  var startDate = new Date(document.getElementById("startDate").value);
+  var endDate = new Date(document.getElementById("endDate").value);
+  var lastBasicWage = parseInt(document.getElementById("lastBasicWage").value);
 
-	// Calculate daily salary
-	var dailySalary = monthlySalary / 30;
+  // Calculate years of service and months of service
+  var yearsOfService = Math.floor((endDate - startDate) / (1000 * 60 * 60 * 24 * 365));
+  var monthsOfService = Math.floor(((endDate - startDate) % (1000 * 60 * 60 * 24 * 365)) / (1000 * 60 * 60 * 24 * 30));
 
-	// Calculate years of service
-	var yearsOfService = (endDate - startDate) / (1000 * 60 * 60 * 24 * 365);
+  // Calculate daily wage
+  var dailyWage = lastBasicWage / 30;
 
-	// Calculate gratuity rate
-	var gratuityRate;
-	if (yearsOfService <= 3) {
-		gratuityRate = 15;
-	} else {
-		gratuityRate = 30;
-	}
+  // Calculate gratuity amount
+  var gratuityAmount = 0;
+  if (yearsOfService < 3) {
+    gratuityAmount = (15 * dailyWage * 30) * yearsOfService + (monthsOfService * dailyWage * 15);
+  } else {
+    gratuityAmount = (30 * dailyWage * 30) * yearsOfService + (monthsOfService * dailyWage * 30);
+  }
 
-	// Calculate fraction of year of service
-	var fractionOfYear = (endDate - startDate) % (1000 * 60 * 60 * 24 * 365) / (1000 * 60 * 60 * 24 * 365);
-
-	// Calculate gratuity amount
-	var gratuityAmount = (gratuityRate / 30) * dailySalary * (yearsOfService + fractionOfYear);
-
-	// Display result
-	document.getElementById("result").innerHTML = "The post service gratuity is: " + gratuityAmount.toFixed(2) + " OMR";
+  // Display result
+  document.getElementById("result").innerHTML = "The post service gratuity is: " + gratuityAmount.toFixed(2) + " OMR";
 }
