d9e7b94c6d27090022e254f03de84c11c7b6d71c
zezokam <125836314+zezokam@users.noreply.github.com>
2023-03-11 10:46:37 +0400

    Update qratuity.js

--- qratuity.js
+++ qratuity.js
@@ -1,29 +1,34 @@
 function calculateGratuity() {
-    // Get input values
-    var startDate = new Date(document.getElementById("startDate").value);
-    var endDate = new Date(document.getElementById("endDate").value);
-    var lastBasicWage = parseInt(document.getElementById("lastBasicWage").value);
+  // Get input values
+  var startDate = new Date(document.getElementById("startDate").value);
+  var endDate = new Date(document.getElementById("endDate").value);
+  var lastBasicWage = parseInt(document.getElementById("lastBasicWage").value);
 
-    // Calculate years and months of service
-    var yearsOfService = endDate.getFullYear() - startDate.getFullYear();
-    var monthsOfService = endDate.getMonth() - startDate.getMonth();
-    if (monthsOfService < 0) {
-        yearsOfService--;
-        monthsOfService += 12;
-    }
+  // Calculate years and months of service
+  var yearsOfService = endDate.getFullYear() - startDate.getFullYear();
+  var monthsOfService = endDate.getMonth() - startDate.getMonth();
+  var daysOfService = endDate.getDate() - startDate.getDate();
+  
+  if (daysOfService >= 15) {
+    monthsOfService++;
+  }
+  
+  if (monthsOfService < 0) {
+    yearsOfService--;
+    monthsOfService += 12;
+  }
 
-    // Calculate daily wage
-    var dailyWage = lastBasicWage / 30;
+  // Calculate daily wage
+  var dailyWage = lastBasicWage / 30;
 
-    // Calculate gratuity amount
-    var gratuityAmount = 0;
-    if (yearsOfService < 3) {
+  // Calculate gratuity amount
+  var gratuityAmount = 0;
+  if (yearsOfService < 3) {
     gratuityAmount = (dailyWage * 15 * yearsOfService) + (dailyWage * monthsOfService / 12 * 30);
-    } else {
+  } else {
     gratuityAmount = (dailyWage * 15 * 3) + (dailyWage * 30) + (dailyWage * 30 * (yearsOfService - 3)) + (dailyWage * monthsOfService / 12 * 30);
-}
+  }
 
-
-    // Display result
-    document.getElementById("result").innerHTML = "The end of service gratuity is: " + gratuityAmount.toFixed(2) + " OMR";
+  // Display result
+  document.getElementById("result").innerHTML = "The end of service gratuity is: " + gratuityAmount.toFixed(2) + " OMR";
 }
