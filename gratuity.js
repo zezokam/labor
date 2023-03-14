8f8bd096d7b590ff06c59b5c06e0c46b182ee664
zezokam <125836314+zezokam@users.noreply.github.com>
2023-03-14 07:31:53 +0400

    Update gratuity.js

--- gratuity.js
+++ gratuity.js
@@ -2,9 +2,9 @@ function calculateGratuity() {
   // Get input values
   var startDate = new Date(document.getElementById("startDate").value);
   var endDate = new Date(document.getElementById("endDate").value);
-  var lastBasicWage = parseFloat(document.getElementById("lastBasicWage").value).toFixed(3);
+  var lastBasicWage = parseFloat(document.getElementById("lastBasicWage").value);
 
-  // Calculate years and months of service
+  // Calculate years, months, and days of service
   var yearsOfService = endDate.getFullYear() - startDate.getFullYear();
   var monthsOfService = endDate.getMonth() - startDate.getMonth();
   var daysOfService = endDate.getDate() - startDate.getDate();
@@ -26,13 +26,46 @@ function calculateGratuity() {
   // Calculate gratuity amount
   var gratuityAmount = 0;
   if (yearsOfService < 3) {
-    gratuityAmount = (dailyWage * 15 * yearsOfService) + (dailyWage * monthsOfService * 30 / 12) + (dailyWage * daysOfService / 12);
+    gratuityAmount = (dailyWage * 15 * yearsOfService) + (dailyWage * monthsOfService * 15 / 12) + (dailyWage * daysOfService / 12);
   } else {
     gratuityAmount = (dailyWage * 15 * 3) + (dailyWage * 30 * (yearsOfService - 3)) + (dailyWage * monthsOfService * 30 / 12) + (dailyWage * daysOfService / 12);
   }
 
+  // Format duration of service
+var yearsText, monthsText, daysText;
+if (yearsOfService == 1) {
+  yearsText = "سنة";
+} else if (yearsOfService == 2) {
+  yearsText = "سنتين";
+} else if (yearsOfService >= 3 && yearsOfService <= 10) {
+  yearsText = "سنوات";
+} else {
+  yearsText = "سنة";
+}
+
+if (monthsOfService == 1) {
+  monthsText = "شهر";
+} else if (monthsOfService == 2) {
+  monthsText = "سنتين"; // Change this line to display "سنتين" instead of "2 سنتين"
+} else if (monthsOfService >= 3 && monthsOfService <= 10) {
+  monthsText = "أشهر";
+} else {
+  monthsText = "شهر";
+}
+
+if (daysOfService == 1) {
+  daysText = "يوم";
+} else if (daysOfService == 2) {
+  daysText = "يومين";
+} else if (daysOfService >= 3 && daysOfService <= 10) {
+  daysText = "أيام";
+} else {
+  daysText = "يوم";
+}
+
+
   // Display result
-  var duration = yearsOfService + " years, " + monthsOfService + " months, " + daysOfService + " days";
-  var resultString = "The end of service gratuity: " + gratuityAmount.toFixed(3) + " OMR." + "<br>" + "Duration of service is: " + duration;
+  var duration = yearsOfService + " " + yearsText + " و " + monthsOfService + " " + monthsText + " و " + daysOfService + " " + daysText;
+  var resultString = "قيمة المكافأة: " + gratuityAmount.toFixed(3) + " ريال" + "<br>" + "مدة الخدمة: " + duration;
   document.getElementById("result").innerHTML = resultString;
 }
