9daf5341b3f0250d66a3b3b333dec2152882d0b9
zezokam <125836314+zezokam@users.noreply.github.com>
2023-03-15 04:46:43 +0400

    Update gratuity.js

--- gratuity.js
+++ gratuity.js
@@ -31,52 +31,51 @@ function calculateGratuity() {
     gratuityAmount = (dailyWage * 15 * 3) + (dailyWage * 30 * (yearsOfService - 3)) + (dailyWage * monthsOfService * 30 / 12) + (dailyWage * daysOfService / 12);
   }
 
-// Format duration of service
-var duration = "";
-if (yearsOfService == 1) {
-  duration += "سنة";
-} else if (yearsOfService == 2) {
-  duration += "سنتين";
-} else if (yearsOfService >= 3 && yearsOfService <= 10) {
-  duration += yearsOfService + " سنوات";
-} else {
-  duration += yearsOfService + " سنة";
-}
-
-if (monthsOfService > 0) {
-  if (duration != "") {
-    duration += " و ";
-  }
-  if (monthsOfService == 1) {
-    duration += "شهر";
-  } else if (monthsOfService == 2) {
-    duration += "شهرين";
-  } else if (monthsOfService >= 3 && monthsOfService <= 10) {
-    duration += monthsOfService + " أشهر";
-  } else if (monthsOfService == 12) {
-    duration += "سنة";
+  // Format duration of service
+  var duration = "";
+  if (yearsOfService == 1) {
+    duration += "1 year";
+  } else if (yearsOfService == 2) {
+    duration += "2 years";
+  } else if (yearsOfService >= 3 && yearsOfService <= 10) {
+    duration += yearsOfService + " years";
   } else {
-    duration += "أشهر";
+    duration += yearsOfService + " year";
   }
-}
 
-if (daysOfService > 0) {
-  if (duration != "") {
-    duration += " و ";
+  if (monthsOfService > 0) {
+    if (duration != "") {
+      duration += " and ";
+    }
+    if (monthsOfService == 1) {
+      duration += "1 month";
+    } else if (monthsOfService == 2) {
+      duration += "2 months";
+    } else if (monthsOfService >= 3 && monthsOfService <= 10) {
+      duration += monthsOfService + " months";
+    } else if (monthsOfService == 12) {
+      duration += "1 year";
+    } else {
+      duration += "months";
+    }
   }
-  if (daysOfService == 1) {
-    duration += "يوم";
-  } else if (daysOfService == 2) {
-    duration += "يومين";
-  } else if (daysOfService >= 3 && daysOfService <= 10) {
-    duration += daysOfService + " أيام";
-  } else {
-    duration += daysOfService + " يوم";
+
+  if (daysOfService > 0) {
+    if (duration != "") {
+      duration += " and ";
+    }
+    if (daysOfService == 1) {
+      duration += "1 day";
+    } else if (daysOfService == 2) {
+      duration += "2 days";
+    } else if (daysOfService >= 3 && daysOfService <= 10) {
+      duration += daysOfService + " days";
+    } else {
+      duration += daysOfService + " day";
+    }
   }
-}
 
   // Display result
-  var duration = yearsOfService + " " + yearsText + " و " + monthsOfService + " " + monthsText + " و " + daysOfService + " " + daysText;
-  var resultString = "قيمة المكافأة: " + gratuityAmount.toFixed(3) + " ريال" + "<br>" + "مدة الخدمة: " + duration;
+  var resultString = "Gratuity Amount: " + gratuityAmount.toFixed(3) + " SAR" + "<br>" + "Duration of Service: " + duration;
   document.getElementById("result").innerHTML = resultString;
 }
